8044784aa737bcd4064068c8581c9741cd2d23e2
Pavithra1707-rt <pavithrart17@gmail.com>
2026-08-29 00:42:06 +0530

    Add sum of natural numbers

---
+++ src/main/java/Week_3/assignment_problems/SumOfNaturalNumbers.java
@@ -0,0 +1,33 @@
+package Week_3.assignment_problems;
+
+
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+
+    static void sumOfNaturalNumbers(int n) {
+
+        int i = 1;
+        int sum = 0;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        sumOfNaturalNumbers(n);
+
+        sc.close();
+    }
+}
