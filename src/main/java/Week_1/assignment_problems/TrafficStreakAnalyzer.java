e6324a0b543f692802e06e3d8eda09364192ed07
Pavithra1707-rt <pavithrart17@gmail.com>
2026-08-29 00:03:11 +0530

    Add Traffic Streak Analyzer

---
+++ src/main/java/Week_1/assignment_problems/TrafficStreakAnalyzer.java
@@ -0,0 +1,45 @@
+package Week_1.assignment_problems;
+
+
+
+import java.util.Scanner;
+
+public class TrafficStreakAnalyzer {
+
+    static void findLongestStreak(String signalLog) {
+
+        char longestColor = signalLog.charAt(0);
+        int longest = 1;
+
+        int current = 1;
+
+        for (int i = 1; i < signalLog.length(); i++) {
+
+            if (signalLog.charAt(i) == signalLog.charAt(i - 1)) {
+                current++;
+            } else {
+                current = 1;
+            }
+
+            if (current > longest) {
+                longest = current;
+                longestColor = signalLog.charAt(i);
+            }
+        }
+
+        System.out.println("Longest Streak: '" + longestColor +
+                "' repeated " + longest + " times");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter signal log: ");
+        String signalLog = sc.nextLine();
+
+        findLongestStreak(signalLog);
+
+        sc.close();
+    }
+}
