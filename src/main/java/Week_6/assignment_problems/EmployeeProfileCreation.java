692eaec66a6ca32d69096df01dfd6d10015b5797
Pavithra1707-rt <pavithrart17@gmail.com>
2026-09-12 16:24:49 +0530

    Add Employee Profile Creation

---
+++ src/main/java/Week_6/assignment_problems/EmployeeProfileCreation.java
@@ -0,0 +1,50 @@
+package Week_6.assignment_problems;
+
+
+
+class Employee {
+
+    String empId;
+    String empName;
+    double salary;
+    boolean isIntern;
+
+    // Permanent employee constructor
+    public Employee(String empId, String empName, double salary) {
+
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.isIntern = false;
+    }
+
+    // Intern constructor
+    public Employee(String empId, String empName) {
+
+        this(empId, empName, 0);
+        this.isIntern = true;
+    }
+
+    public void printProfile() {
+
+        System.out.println(empId + " | " +
+                empName + " | Rs " +
+                salary + " | Intern: " +
+                isIntern);
+    }
+}
+
+public class EmployeeProfileCreation {
+
+    public static void main(String[] args) {
+
+        Employee permanent =
+                new Employee("E-101", "Divya", 65000);
+
+        Employee intern =
+                new Employee("E-102", "Arjun");
+
+        permanent.printProfile();
+        intern.printProfile();
+    }
+}
