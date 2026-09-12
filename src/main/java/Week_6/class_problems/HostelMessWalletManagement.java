ffb4d4968e83706dae671a072ad8015c579236d2
Pavithra1707-rt <pavithrart17@gmail.com>
2026-09-12 16:01:06 +0530

    Add Hostel Mess Wallet Management

---
+++ src/main/java/Week_6/class_problems/HostelMessWalletManagement.java
@@ -0,0 +1,57 @@
+package Week_6.class_problems;
+
+
+
+
+class MessWallet {
+
+    private double balance;
+
+    public MessWallet(double openingBalance) {
+
+        if (openingBalance < 0) {
+            balance = 0;
+            System.out.println("Invalid opening balance. Starting at 0.");
+        } else {
+            balance = openingBalance;
+        }
+    }
+
+    public void topUp(double amount) {
+
+        if (amount <= 0) {
+            System.out.println("Top-up amount must be greater than 0");
+        } else {
+            balance = balance + amount;
+            System.out.println("Balance after top-up: " + balance);
+        }
+    }
+
+    public void deduct(double amount) {
+
+        if (amount > balance) {
+            System.out.println("Deduct rejected: insufficient balance");
+        } else {
+            balance = balance - amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class HostelMessWalletManagement {
+
+    public static void main(String[] args) {
+
+        MessWallet wallet = new MessWallet(500);
+
+        wallet.topUp(200);
+
+        wallet.deduct(1000);
+
+        System.out.println("Final balance: " +
+                wallet.getBalance());
+    }
+}
